f501eaa7dcd5143255bc7ee2dae0f469f0d7c15a
Bek U <ustabekovb@gmail.com>
2019-07-15 21:22:38 -0500

    jenkins 1

---
+++ src/jenkins1/jenkins1.java
@@ -0,0 +1,11 @@
+package jenkins1;
+
+public class jenkins1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Jenkins running 1");
+        }
+    }
+}
